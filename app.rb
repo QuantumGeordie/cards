c7bc66aaa0e9c3dc1943acf564a8ec4cfa147d1b
geordie <george.speake@gmail.com>
2013-01-13 00:32:51 -0800

    if i weren't so dumb, i'd modify the right configuration. this time i should be using memory db for 'test'.

--- app.rb
+++ app.rb
@@ -21,13 +21,13 @@ configure :development do
 end
 
 configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:memory")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/db.sqlite3")
   Neo.setup("http://localhost:7474")
 end
 
 configure :test do
   # DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/db.test.sqlite3")
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/db.test.sqlite3")
+  DataMapper.setup(:default, "sqlite3:memory")
   Neo.setup("http://localhost:7476")
 end
 
